4d30d8048b8c1292c4bc535566405c56eb5dcbae
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-18 20:39:28 -0300

    show follow/unfollow

--- src/Components/shared/Header/Header.js
+++ src/Components/shared/Header/Header.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import UserContext from "../../../contexts/UserContext";
@@ -5,7 +6,6 @@ import InputSearchUsers from "../../InputSearch/InputSearchUsers";
 import { useNavigate } from "react-router-dom";
 import ClickAwayListener from "react-click-away-listener";
 import UrlContext from "../../../contexts/UrlContext";
-import { Container, Logout, Profile, Title } from "./HeaderStyle";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -21,18 +21,18 @@ export default function Header() {
         Authorization: `Bearer ${token}`,
       },
     };
-    const user = axios.get(`${URL}/me`, config);
-    user
+    const promise = axios.get(`${URL}/me`, config);
+    promise
       .then((res) => {
         setUserData(res.data);
-        localStorage.setItem("userLinkerId",userData[0].id);
+        localStorage.setItem("userLinkerId", res.data[0].id);
       })
       .catch((error) => {
         alert(`Erro: \n\n${error.response.status} - ${error.response.data}`);
         localStorage.removeItem("tokenLinker");
         navigate("/");
       });
-  }, [URL, navigate, token, setUserData, userData]);
+  }, [URL, navigate, token, setUserData]);
   function logout() {
     localStorage.removeItem("tokenLinker");
     navigate("/");
@@ -67,4 +67,79 @@ export default function Header() {
   );
 }
 
+const Logout = styled.div`
+  position: absolute;
+  width: 150px;
+  height: 47px;
+  top: 80px;
+  right: 0;
+  background: #151515;
+  border-radius: 0 0 0 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  p {
+    width: 57px;
+    height: 20px;
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    color: #ffffff;
+  }
+`;
 
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  width: 150px;
+  background-color: #151515;
+  justify-content: space-evenly;
+  img {
+    width: 53px;
+    height: 53px;
+    border-radius: 26.5px;
+    object-fit: cover;
+  }
+`;
+
+const Title = styled.div`
+  width: 108px;
+  height: 54px;
+  font-family: "Passion One";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 49px;
+  line-height: 54px;
+  letter-spacing: 0.05em;
+  color: #ffffff;
+  cursor: pointer;
+  margin-left: 20px;
+`;
+
+const Container = styled.div`
+  font-family: "Passion One", cursive;
+  position: fixed;
+  z-index: 2;
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 75px;
+  background-color: #151515;
+  color: #ffffff;
+  padding: 10px 0;
+  display: flex;
+  align-items: flex-start;
+  box-sizing: content-box;
+  @media (max-width: 563px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 100vw;
+    padding: 0;
+  }
+`;
