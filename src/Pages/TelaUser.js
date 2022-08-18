c88ef41bb59b8347241e99c8d16eaf812e29fa0f
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-18 13:19:26 -0300

    merged with follow

--- src/Pages/TelaUser.js
+++ src/Pages/TelaUser.js
@@ -1,135 +1,156 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import axios from "axios"
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
-import { useEffect, useState, useContext} from "react";
-// import styled from "styled-components";
-import Post from "../Components/Posts/Post/Post"
+import { useEffect, useState, useContext } from "react";
+import Post from "../Components/Posts/Post/Post";
 import { FeedPage } from "../Components/shared/Feed/FeedPage";
 import UrlContext from "../contexts/UrlContext";
+import { Follow, Unfollow } from "../Components/shared/Feed/FeedStyle";
 
+export default function TelaUser() {
+  const URL = useContext(UrlContext);
+  const userLoggedId = parseInt(localStorage.getItem("userLinkerId"));
+  const [postList, setPostList] = useState(null);
+  const [hashtags, setHashtags] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [loadingFollow, setLoadingFollow] = useState(false);
+  const [name, setName] = useState("");
+  const [isFriend, setIsFriend] = useState(false);
 
+  const token = localStorage.getItem("tokenLinker");
+  const { id } = useParams();
 
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    setLoading(true);
+    const promise = axios.get(`${URL}/posts/${id}`, config);
+    promise.then((response) => {
+      setPostList(response.data);
+    });
+    promise.catch((error) => {
+      alert("Erro ao pegar os posts");
+    });
 
-export default function TelaUser(){
-    const URL = useContext(UrlContext);
-    const [postList, setPostList] = useState(null);
-    const [hashtags, setHashtags] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [name, setName] = useState('')
-    // let name;
-    // if (postList != null && postList.length > 0) {
-    //     console.log('entrou no if nao nulo')
-    //     name = postList[0].userOwner.name
-    // }
-    
-    const token = localStorage.getItem('tokenLinker');
-    console.log(token)
-    const { id } = useParams();
+    axios
+      .get(`${URL}/user/${id}`, config)
+      .then((res) => {
+        console.log(res.data[0]);
+        setName(res.data[0].name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    useEffect(() => {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+    axios
+      .get(`${URL}/hashtags`, config)
+      .then((res) => {
+        const arrayHashtags = [];
+        for (const hash of res.data) {
+          arrayHashtags.push(hash.text);
         }
-        setLoading(true);
-        const promise = axios.get(`${URL}/posts/${id}`,config);
-        promise.then(response => {
-            setPostList(response.data);
-
-        });
-        promise.catch((error) => {
-            console.log(error.response.data);
-            alert("Erro ao pegar os posts");
-
-        });
-
-        axios.get(`${URL}/user/${id}`, config)
-            .then(res=>{
-                console.log(res.data[0])
-                setName(res.data[0].name)
-            })
-            .catch(err=>{
-                console.log(err)
-            })
-
-        axios.get(`${URL}/hashtags`, config)
-            .then(res => {
-                const arrayHashtags = [];
-                for (const hash of res.data) {
-                    arrayHashtags.push(hash.text);
-                }
-                setHashtags([...arrayHashtags]);
-            })
-            .catch(err => {
-                console.log(err.response.data);
-                alert("An error occured while trying to fetch the trenddins, please refresh the page");
-                setLoading(false);
-            });
-
-
-    }, [token, id, URL]);
-
-
-    function postsList(openModal) {
-        return (
-            postList?.length > 0 ?
-                postList.map((p, index) =>
-                    <Post
-                        key={index}
-                        id={p.postId}
-                        userOwner={p.userOwner}
-                        urlData={p.urlData}
-                        description={p.description}
-                        likesCount={p.likesCount}
-                        likes={p.likes}
-                        openModal={openModal}
-                        idUser={p.userOwner.id}
-                    />
-                )
-                :
-                loading || !postList || !hashtags ?
-                    <p>There are no posts yet</p>
-                    :
-                    <ThreeDots color="#FFF" height={50} width={100} />
-
+        setHashtags([...arrayHashtags]);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        alert(
+          "An error occured while trying to fetch the trenddins, please refresh the page"
         );
-    }
+        setLoading(false);
+      });
 
-    return (
-        <FeedPage title={`${name}'s Posts`} posts={postsList} hashtags={hashtags}  />
-    )
+    const friends = axios.get(`${URL}/follow`, config);
+    friends
+      .then((res) => {
+        if (res.data.filter((friendId) => friendId === parseInt(id)).length > 0)
+          setIsFriend(true);
+      })
+      .catch((e) => alert(e.message));
+  }, [token, id, URL, isFriend]);
+
+  function follow() {
+    setLoadingFollow(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const promise = axios.post(`${URL}/follow/${parseInt(id)}`, {}, config);
+    promise
+      .then((res) => {
+        setIsFriend(true);
+        setLoadingFollow(false);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
+  function unfollow() {
+    setLoadingFollow(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const promise = axios.delete(`${URL}/follow/${id}`, config);
+    promise
+      .then((res) => {
+        setIsFriend(false);
+        setLoadingFollow(false);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
+  function postsList(openModal) {
+    return postList?.length > 0 ? (
+      postList.map((p, index) => (
+        <Post
+          key={index}
+          id={p.postId}
+          userOwner={p.userOwner}
+          urlData={p.urlData}
+          description={p.description}
+          likesCount={p.likesCount}
+          likes={p.likes}
+          openModal={openModal}
+          idUser={p.userOwner.id}
+        />
+      ))
+    ) : loading || !postList || !hashtags ? (
+      <p>There are no posts yet</p>
+    ) : (
+      <ThreeDots color="#FFF" height={50} width={100} />
+    );
+  }
+
+  return (
+    <>
+      <FeedPage
+        title={`${name}'s Posts`}
+        posts={postsList}
+        hashtags={hashtags}
+        isFriend={isFriend}
+      />
+      {userLoggedId !== parseInt(id) ? (
+        loadingFollow ? (
+          <Follow>
+            <ThreeDots color="#FFF" height={50} width={100} />
+          </Follow>
+        ) : isFriend ? (
+          <Unfollow onClick={unfollow}>Unfollow</Unfollow>
+        ) : (
+          <Follow onClick={follow}>Follow</Follow>
+        )
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
-
-
-
-
-// const Container = styled.div`
-// background-color:#333333;
-// height:100%;
-// width:100%;
-// display:flex;
-// flex-direction:column;
-// align-items:center;
-// box-sizing:border-box;
-// padding:100px 0 500px 0;
-// h1{
-//     font-family: 'Oswald', sans-serif;
-//     font-size:44px;
-//     font-weight:bold;
-//     margin:30px 0;
-//     width:50%;
-//     color:#FFFFFF;
-// }
-
-// @media (max-width: 1130px) {
-//     padding-top: 0;
-//     h1{
-//         font-size:34px;
-//         width:100%;
-//         margin:20px 0;
-//     }
-//   }
-
-// `
